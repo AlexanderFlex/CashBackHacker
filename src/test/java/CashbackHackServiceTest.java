9eb6b5a23d9bf894c6c3dfd9e65d12ebe7fd1159
Alexander Flex <ronion@yandex.ru>
2023-02-05 20:09:59 +0300

    fix test

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,4 +1,36 @@
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
+
 public class CashbackHackServiceTest {
-    
+
+    @Test
+    public void testRemainIfAmountLess1000() {
+        int amount = 900;
+        int expected = 100;
+        CashbackHackService hack = new CashbackHackService();
+        int actual = hack.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void testRemainIfAmountMore1000() {
+        int amount = 1900;
+        int expected = 100;
+        CashbackHackService hack = new CashbackHackService();
+        int actual = hack.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void testRemainIfAmountEqual1000() {
+        int amount = 1000;
+        int expected = 0;
+        CashbackHackService hack = new CashbackHackService();
+        int actual = hack.remain(amount);
+        assertEquals(actual, expected);
+
+    }
 }
